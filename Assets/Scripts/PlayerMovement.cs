c991acea1fbef88bc2a90cee00bd0c68b4a071fa
Leah Zahn <zahn@chapman.edu>
2021-04-01 23:28:00 -0700

    finished all videos but having weird jumping issues

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 25f;
+    public bool hasJumpPotion = false;
+    public bool hasSpeedPotion = false;
+    public int potionModAmount = 0;
+
+    public AudioClip jumpClip;
+
+    private float potionTimeMax = 10f;
+    private float potionTimeCur = 0f;
+
+    float horizontalMove = 0f;
+    bool jumpFlag = false;
+    bool jump = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        BoundMovement();
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (jumpFlag)
+        {
+            animator.SetBool("IsJumping", true);
+            jumpFlag = false;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (animator.GetBool("IsJumping") == false)
+            {
+                AudioSource.PlayClipAtPoint(jumpClip, transform.position);
+                jump = true;
+                animator.SetBool("IsJumping", true);
+            }            
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (hasJumpPotion && potionTimeCur < potionTimeMax)
+        {
+            controller.m_JumpForceMod = potionModAmount;
+            potionTimeCur += Time.fixedDeltaTime;
+        }
+        else
+        {
+            potionTimeCur = 0;
+            controller.m_JumpForceMod = 0;
+            hasJumpPotion = false;
+        }
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        if (jump)
+        {
+            jumpFlag = true;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+        jump = false;
+    }
+
+    private void BoundMovement()
+    {
+        float dist = (this.transform.position - Camera.main.transform.position).z;
+        float leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+        float rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+        float topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
+        float bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
+
+        Vector3 playerSize = GetComponent<Renderer>().bounds.size;
+
+        this.transform.position = new Vector3(
+        Mathf.Clamp(this.transform.position.x, leftBorder + playerSize.x / 2, rightBorder - playerSize.x / 2),
+        Mathf.Clamp(this.transform.position.y, topBorder + playerSize.y / 2, bottomBorder - playerSize.y / 2),
+        this.transform.position.z
+            );
+    }
+}
